cd537764f75549f896eb27579442d0707db05772
Tolstovku <derexdahik@yandex.ru>
2020-03-10 21:10:18 +0300

    Add script for lib Add gitignore

---
+++ pa2_DC/for-lib-to-work.sh
@@ -0,0 +1,4 @@
+# говно из методы которое нужно чтобы либа с временем заработала
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/path/to/pa2/dir";
+### пустая строка
+LD_PRELOAD=/full/path/to/libruntime.so ./pa2 –p 2 10 20
