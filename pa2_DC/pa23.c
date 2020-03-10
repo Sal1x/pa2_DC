3ca5374d8aa02c0b1c5236dfe59bb93a8e4d9400
Sal1x <mr.verbovoy@mail.ru>
2020-03-10 20:26:23 +0300

    Add files via upload

---
+++ pa2_DC/pa23.c
@@ -0,0 +1,42 @@
+#include "banking.h"
+
+void transfer(void * parent_data, local_id src, local_id dst,
+              balance_t amount)
+{
+    // student, please implement me
+}
+
+int main(int argc, char * argv[])
+{
+    size_t num_children;
+    size_t num_processes;
+    balance_t initial_balances[MAX_PROCESS];
+
+
+    if (argc >= 3 && strcmp(argv[1], "-p") == 0) {
+        num_children = strtol(argv[2], NULL, 10);
+        num_processes = num_children + 1;
+
+        if (num_children >= MAX_PROCESSES) {
+            fprintf(stderr, "ERROR: Too many children requested.\n");
+            return 1;
+        }
+
+        if (argc != 3 + num_children) {
+            fprintf(stderr, "ERROR: Expected %ld balances after `%s %s'\n",
+                    num_children, argv[1], argv[2]);
+            return 1;
+        }
+
+        for (size_t i = 1; i <= num_children; i++) {
+            initial_balances[i] = strtol(argv[2 + i], NULL, 10);
+        }
+    } else {
+        fprintf(stderr, "ERROR: Key '-p NUMBER_OF_CHILDREN' is mandatory\n");
+        return 1;
+    }
+    //bank_robbery(parent_data);
+    //print_history(all);
+
+    return 0;
+}
